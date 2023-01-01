506d3a00bdf5c726f0c9c727f965feb5a46616fc
Steven McSorley <steven.mcsorley@eqtr.com>
2023-01-01 18:06:17 +0000

    added small node server to parse the data,json and show the results in google styled search results

--- server.js
+++ server.js
@@ -1,19 +1,42 @@
 const http = require('http');
 const fs = require('fs');
-const port = 8000;
+const url = require('url');
 
-http.createServer((req, res) => {
-  fs.readFile('index.html', (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(data);
-  });
-}).listen(port);
+const server = http.createServer((req, res) => {
+  // Parse the request URL
+  const parsedUrl = url.parse(req.url);
 
+  // Use a switch statement to determine which file to serve
+  switch (parsedUrl.pathname) {
+    case '/':
+      // Set the response type
+      res.setHeader('Content-Type', 'text/html');
 
+      // Read the HTML file and send it as the response
+      fs.readFile('index.html', (err, data) => {
+        res.end(data);
+      });
+      break;
+    case '/data.json':
+      // Set the response type
+      res.setHeader('Content-Type', 'application/json');
 
-console.log(`Server running at http://localhost:${port}/`);
+      // Read the JSON file and send it as the response
+      fs.readFile('data.json', (err, data) => {
+        res.end(data);
+      });
+      break;
+    default:
+      // Set the response status and type
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+
+      // Send a "not found" message as the response
+      res.end('404 Not Found');
+      break;
+  }
+});
+
+server.listen(3000);
+
+console.log('Server listening on port 3000');
